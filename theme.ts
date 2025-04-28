bffc31596fdc41e5a849bc97b8ed4b9624b6fd48
my200020002 <158255535+my200020002@users.noreply.github.com>
2025-04-28 20:34:37 +0800

    Create theme.ts

---
+++ theme.ts
@@ -0,0 +1,51 @@
+import { type GlobalThemeOverrides } from 'naive-ui'
+const themeOverrides: GlobalThemeOverrides = {
+  common: {
+    //按钮悬浮框点击的扩展色
+    primaryColor: '#4C76F7',
+    //按钮悬浮框颜色
+    primaryColorHover: '#6989F8',
+    //按钮悬浮点击框颜色
+    primaryColorPressed: '#3A67E6',
+    primaryColorSuppl: '#4C76F7',
+
+    // 衍生色
+    // infoColor: '#4C76F7',
+    // infoColorHover: '#6989F8',
+    // infoColorPressed: '#3A67E6',
+    // infoColorSuppl: '#4C76F7',
+  },
+  Layout: {
+    headerColor: '#071528',
+    siderColor: '#071528',
+  },
+  Menu: {
+    itemTextColor: '#808080',
+    itemTextColorHover: "#ffffff",
+    itemTextColorActive: "#ffffff",
+    itemTextColorActiveHover: "#ffffff",
+    itemTextColorChildActive: "#ffffff",
+    itemTextColorChildActiveHover: "#ffffff",
+
+    itemColorHover: "#071528",
+    itemColorActive: "#4C76F7",
+    itemColorActiveHover: "#4C76F7",
+
+    itemIconColor: '#808080',
+    itemIconColorHover: "#ffffff",
+    itemIconColorActive: "#ffffff",
+    itemIconColorActiveHover: "#ffffff",
+    itemIconColorChildActive: "#ffffff",
+    itemIconColorChildActiveHover: "#ffffff",
+
+    arrowColor: '#808080',
+    arrowColorHover: "#ffffff",
+    arrowColorActive: "#ffffff",
+    arrowColorActiveHover: "#ffffff",
+    arrowColorChildActive: "#ffffff",
+    arrowColorChildActiveHover: "#ffffff",
+
+
+  }
+
+}
